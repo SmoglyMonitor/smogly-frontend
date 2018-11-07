eb53cc21c386991191c58dbe15122e562360a819
Kamil Dzieniszewski <kamil.dzieniszewski@daftcode.pl>
2018-11-07 18:22:12 +0100

    cleanup

--- app/components/DataHolder.jsx
+++ app/components/DataHolder.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 export default class Data extends React.Component {
     render () {
-        return (<div className="data">
+        return (
+        <div className="data">
             I'm data
         </div>)
     }
